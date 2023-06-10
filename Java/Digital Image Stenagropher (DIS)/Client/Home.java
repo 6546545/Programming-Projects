31b214716444eb3c6bfc47d75bcf5dc4bcb7d45f
6546545 <andy.gould46@gmail.com>
2023-06-10 15:41:25 -0400

    Initial setup of project.

---
+++ Java/Digital Image Stenagropher (DIS)/Client/Home.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Home extends JFrame implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }}
