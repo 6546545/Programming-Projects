50e4e7ba1859442d056fd15fc394190cbbe55f09
6546545 <andy.gould46@gmail.com>
2023-06-25 09:51:35 -0400

    ...

--- JAVA/Java Web App/User.java
+++ JAVA/Java Web App/User.java
@@ -2,42 +2,83 @@
  * User
  */
 public class User {
-    private int id;
-    private String name, email, country;
+    protected int id;
+    protected String name, email, country;
 
-    public User() {
-        
-    }
-    public User(String nameString, String emailString, String countryString){
-
+    public User(){
 
     }
-    public User(int id, String nameString, String emailString, String countryString){
-        
+    /**
+     * @param name
+     * @param email
+     * @param country
+     */
+    public User(String name, String email, String country) {
+        super();
+        this.name = name;
+        this.email = email;
+        this.country = country;
     }
 
+    /**
+     * @param id
+     * @param name
+     * @param email
+     * @param country
+     */
+    public User(int id, String name, String email, String country) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.country = country;
+    }
     
+    /**
+     * @return
+     */
     public int getId() {
         return id;
     }
+    /**
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }    
+    /**
+     * @return
+     */
     public String getCountry() {
         return country;
     }
+    /**
+     * @param country
+     */
     public void setCountry(String country) {
         this.country = country;
     }
+    /**
+     * @return
+     */
     public String getEmail() {
         return email;
     }
+    /**
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
+    /**
+     * @return
+     */
     public String getName() {
         return name;
     }
+    /**
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
