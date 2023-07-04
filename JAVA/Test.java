56bb32ee4445e0e7b38f459258e20573f1e3804a
6546545 <andy.gould46@gmail.com>
2023-07-03 20:36:40 -0400

    Added main loop.

--- JAVA/Test.java
+++ JAVA/Test.java
@@ -1,7 +1,29 @@
 import OOP.Planet;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.TextArea;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.text.JTextComponent;
+
 public class Test {
     public static void main(String[] args) {
+        JFrame window = new JFrame("Test Window");
+        JTextField title = new JTextField("Title");
+        //title.setLocation(window.getWidth()-(0.5 * window.getWidth()), window.getHeight()-(0.5 * window.getHeight()));
+        title.setEditable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.add(title, 0);
+        window.setSize(1920, 1080);
+        window.setVisible(true);
+
+
+    
+
+
         Planet x= new Planet();
 
         x.setGasGiant(false);
