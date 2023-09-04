a4aa2a24eabdd4400d8f07f2acef20cb7ee40548
6546545 <andy.gould46@gmail.com>
2023-09-04 16:28:02 -0400

    Refactored, Implemented (';', '[]' , '{}') in Lexer

---
+++ JACA (JAVA+C)/src/Token.java
@@ -0,0 +1,73 @@
+package src;
+public class Token {
+    public enum TokenType {
+        //Var Types
+        INTEGER,//Added to Lexer
+        FLOAT,
+        STRING, // Added to Lexer
+        BOOL,
+        OBJECT,
+        ARRAY,
+        BINTREE,
+        DICT,
+        HASHMAP,
+        CHAR,
+        LONG,
+        VOID,
+        DOUBLE,
+        ENUM,
+        BYTE,
+        STATIC,
+        VOLITILE,
+        KEYWORD, //Potential for new Keywords to be added?
+        AUTO,
+        ABSTRACT,
+        THROW,
+        CATCH,
+        DO,
+        
+
+        //Operators
+        PLUS, //Added to Lexer
+        MINUS, //Added to Lexer
+        MULTIPLY, //Added to Lexer
+        DIVIDE, //Added to Lexer
+        LPARENTH, //Added to Lexer
+        RPARENTH, //Added to Lexer
+
+        //Program Flow
+        IF,
+        ELSE,
+        WHILE,
+        RETURN,
+        FOR,
+        ELIF,
+        MEM, //Memory Management
+        SWITCH,
+        CASE,
+        BREAK,
+        SEMICOLON, //Added to Lexer               End of Line
+        EOF, // End of File
+        LBRACKET,
+        RBRACKET,
+        LCBRACKET,
+        RCBRACKET,
+        OR,
+        AND,
+        //SPECIAL
+        CLASS
+    }
+
+    public TokenType type;
+    public String value;
+
+    public Token(TokenType type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + type + ", " + value + ")";
+    }
+}
