02ff1b4c52959148ca73eb32db02a8d3e9891b09
6546545 <andy.gould46@gmail.com>
2023-09-04 17:47:48 -0400

    Added comments to Lexer

--- JACA (JAVA+C)/src/Lexer.java
+++ JACA (JAVA+C)/src/Lexer.java
@@ -1,5 +1,6 @@
 package src;
 /*
+ * @6546545
  * 
  * This Lexer class performs lexical analysis of input source code
  *          and breaks it down into tokens.
@@ -9,20 +10,36 @@ package src;
  * 
  */
 public class Lexer {
+    
+    //Private Variables
     private String input;
     private int position = 0;
 
+    /*Constructor
+     * @param String
+     */
     public Lexer(String input) {
         this.input = input;
     }
 
+    /* Next Token 
+     *  Returns the next Token 
+     * 
+     * 
+     * @return Token
+     * @throws IllegalArgumentException
+     */
     public Token nextToken() {
+
+        //End of File
         if (position >= input.length()) {
             return new Token(Token.TokenType.EOF, "");
         }
 
+        //Set currentChar
         char currentChar = input.charAt(position);
 
+        //Creates new Token if char mataches Token
         if (Character.isDigit(currentChar)) {
             return readInteger();
         } else if(Character.isAlphabetic(currentChar)){
@@ -71,6 +88,11 @@ public class Lexer {
         throw new IllegalArgumentException("Invalid character: " + currentChar);
     }
 
+    /* Read Integer
+     *  Translates the Int into appropriate Token, then returns new Token to nextToken()
+     * 
+     * @return Token
+     */
     private Token readInteger() {
         StringBuilder sb = new StringBuilder();
         while (position < input.length() && Character.isDigit(input.charAt(position))) {
@@ -79,6 +101,12 @@ public class Lexer {
         }
         return new Token(Token.TokenType.INTEGER, sb.toString());
     }
+
+     /* Read Alphas
+     *  Translates the alphas into appropriate Token, then returns new Token to nextToken()
+     * 
+     * @return Token
+     */
     private Token readAlpha(){
         StringBuilder sb = new StringBuilder();
         while(position < input.length() && Character.isAlphabetic(input.charAt(position))){
