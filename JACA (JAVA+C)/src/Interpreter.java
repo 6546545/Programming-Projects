3f0bccb8326056033cca588e0e986dd1bc33a47c
6546545 <andy.gould46@gmail.com>
2023-09-04 17:52:08 -0400

    Added comments to Intepreter

--- JACA (JAVA+C)/src/Interpreter.java
+++ JACA (JAVA+C)/src/Interpreter.java
@@ -2,23 +2,38 @@ package src;
 import java.util.HashMap;
 import java.util.Map;
 /*
+ * @6546545
  * 
- * This Interpreter class performs 
+ * This Interpreter class performs the Interpretation of the code.
  * 
  * 
  * 
  */
 public class Interpreter {
+    //Variables
     Map<String, Integer> variables;
 
+    //Constructor
     public Interpreter() {
         variables = new HashMap<>();
     }
 
+    /* Assign
+     *    Assigns the variable name and value of Map<S,I>
+     * 
+     * @param String, int
+     * 
+     */
     public void assign(String varName, int value) {
         variables.put(varName, value);
     }
 
+    /* Execute
+     *    Executes the digestion of the code.
+     * 
+     * 
+     * @param String code
+     */
     public void execute(String code) {
         String[] lines = code.split("\n");
         for (String line : lines) {
