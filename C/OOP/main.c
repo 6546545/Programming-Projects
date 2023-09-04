99eb7547f3bc3d3ec18df29de7933e3ff1643d81
6546545 <andy.gould46@gmail.com>
2023-09-04 15:45:21 -0400

    Added some comments and IO statements.

--- C/OOP/main.c
+++ C/OOP/main.c
@@ -2,5 +2,12 @@
 
 int main(void){
     printf("Hello World!");
+    //Variables are like Java variables i.e. "varType varName"
+    int age;
+    age = 10;
 
+    printf("Enter your age:\n");
+
+    scanf("%i", &age);
+    printf("Your age is %i\n", age);
 }
