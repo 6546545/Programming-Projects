ab2c072d1ac803f5763d0fe2ae96af5aaaf86bb1
6546545 <andy.gould46@gmail.com>
2023-09-07 14:47:55 -0400

    Created Cyrus project.

---
+++ JAVA/New Programming Lanuage/Cyrus.java
@@ -0,0 +1,15 @@
+public class Cyrus {
+    String code;
+
+    public Cyrus(String code){
+        this.code = code;
+    }
+
+    public String tokenize(){
+        int len = this.code.length();
+        int pos = 0;
+        String[] tokens;
+        String[] KEYWORDS = {"print", "for", "while"};
+        
+    }
+}
