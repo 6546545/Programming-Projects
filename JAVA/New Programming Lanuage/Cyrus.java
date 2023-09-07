d99a1c680551f2321bab40b2335816f84d7d9eca
6546545 <andy.gould46@gmail.com>
2023-09-07 16:33:42 -0400

    Added constructors and methods to Cyrus.java

--- JAVA/New Programming Lanuage/Cyrus.java
+++ JAVA/New Programming Lanuage/Cyrus.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Cyrus {
     String code;
 
@@ -5,11 +7,59 @@ public class Cyrus {
         this.code = code;
     }
 
-    public String tokenize(){
+    public HashMap<Object, String> tokenize(){
         int len = this.code.length();
         int pos = 0;
-        String[] tokens;
-        String[] KEYWORDS = {"print", "for", "while"};
+        HashMap<Object, String> tokens = new HashMap<Object, String>();
+        String[] KEYWORDS = {"print"};
+
+        CharSequence allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
         
+        while (pos < len) {
+            char currChar = this.code.charAt(pos);
+
+
+            if (currChar == ' ' || currChar == '\n') {
+                pos++;
+                continue;
+            } else if (currChar == '"') {
+                String tempString = "";
+                pos++;
+
+                while (this.code.charAt(pos) != '"' && this.code.charAt(pos) != '\n' && pos < len) {
+                    tempString += this.code.charAt(pos);
+                    pos++;
+                }
+                if (this.code.charAt(pos) != '"') {
+                    throw new Error("String is not valid.");
+                }
+                pos++;
+                tokens.put("string", tempString);
+            } else if (allowedChars.toString().contains(String.valueOf(currChar))) {
+                String tempString = String.valueOf(currChar);
+                pos++;
+
+                while (allowedChars.toString().matches(String.valueOf(this.code.charAt(pos))) && pos < len) {
+                    tempString += this.code.charAt(pos);
+                }
+                if (!KEYWORDS.toString().matches(tempString)) {
+                    throw new Error("Unexpected token: " + tempString);
+                }
+                tokens.put("keyword", tempString);
+            }
+            
+        }
+        return tokens;
+    }
+    public void run(){
+        HashMap<Object, String> newMap = new HashMap<Object,String>();
+        String newTokenERROR = this.tokenize().toString();
+        //newMap.put(newTokenERROR, newTokenERROR);
+        System.out.println(newTokenERROR);
+    }
+
+    public static void main(String[] args) {
+    Cyrus newCyrus = new Cyrus("print");
+    newCyrus.run();
     }
 }
