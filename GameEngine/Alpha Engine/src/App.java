849708b5cb18158f413bb50223dfba70aad405b1
6546545 <andy.gould46@gmail.com>
2023-12-28 17:04:15 -0500

    added LWJGL library

--- GameEngine/Alpha Engine/src/App.java
+++ GameEngine/Alpha Engine/src/App.java
@@ -18,7 +18,10 @@ import static org.lwjgl.system.MemoryUtil.*;
 public class App {
     private long window;
     
-    public void run(){}
+    public void run(){
+        init();
+
+    }
 
     public void init(){
         GLFWErrorCallback.createPrint(System.err).set();
@@ -30,15 +33,55 @@ public class App {
         GLFW.glfwDefaultWindowHints();
         GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE);
         GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE);
+
+        window = GLFW.glfwCreateWindow(640, 480, "Alpha Engine", NULL, NULL);
+        if (window == NULL) {
+            throw new IllegalStateException("Unable to create GLFW Window");
+        }
+        GLFW.glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {});
+        try (MemoryStack stack = stackPush()){
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+
+            GLFW.glfwGetWindowSize(window, pWidth, pHeight);
+
+            GLFWVidMode vidMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+
+            GLFW.glfwSetWindowPos(window, (vidMode.width() - pWidth.get(0)) / 2, (vidMode.height() - pHeight.get(0)) / 2);
+
+            GLFW.glfwMakeContextCurrent(window);
+            GLFW.glfwSwapInterval(1);
+            GLFW.glfwShowWindow(window);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+
+
     }
 
-    public void loop(){}
+    public void loop(){
+        GL.createCapabilities();
+
+        while (!GLFW.glfwWindowShouldClose(window)) {
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);
+            GLFW.glfwSwapBuffers(window);
+            GLFW.glfwPollEvents();
+        }
+        Callbacks.glfwFreeCallbacks(window);
+        GLFW.glfwDestroyWindow(window);
+
+        GLFW.glfwTerminate();
+        GLFW.glfwSetErrorCallback(null).free();
+
+    }
 
 
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Window window = new Window();
-        window.CreateWindow();
+        new App().run();
+        //Window window = new Window();
+        //window.CreateWindow();
     }
 }
