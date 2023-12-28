437a2ec743c52de8b74baf97a7b49a29101e1218
6546545 <andy.gould46@gmail.com>
2023-12-28 11:38:29 -0500

    Fix: Fix window dimension issues when creating a JFrame

--- GameEngine/Alpha Engine/lib/graphics/window/Window.java
+++ GameEngine/Alpha Engine/lib/graphics/window/Window.java
@@ -6,9 +6,14 @@ import javax.swing.*;
  */
 public class Window {
 
-    private static void CreateWindow(){
+    public void CreateWindow(){
         JFrame frame = new JFrame("Alpha");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(0,  0, 500, 500);
+
+        JLabel label = new JLabel("Test", SwingConstants.CENTER);
+        label.setPreferredSize(new Dimension(300,100));
+        frame.getContentPane().add(label, BorderLayout.CENTER);
 
         frame.setLocationRelativeTo(null);
         frame.pack();
