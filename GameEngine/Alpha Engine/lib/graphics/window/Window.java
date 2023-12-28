0f9e584578efff7fe0cabaa486f9df20e2d6d584
6546545 <andy.gould46@gmail.com>
2023-12-28 11:25:24 -0500

    Initialized Window class

--- GameEngine/Alpha Engine/lib/graphics/window/Window.java
+++ GameEngine/Alpha Engine/lib/graphics/window/Window.java
@@ -1,2 +1,17 @@
 import java.awt.*;
 import javax.swing.*;
+
+/**
+ * Window
+ */
+public class Window {
+
+    private static void CreateWindow(){
+        JFrame frame = new JFrame("Alpha");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
