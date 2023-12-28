c0cce621c7c4ee6d45bdcf23bcb1a120ca31ea72
6546545 <andy.gould46@gmail.com>
2023-12-28 16:25:21 -0500

    Added GLFW functions

--- GameEngine/Alpha Engine/src/App.java
+++ GameEngine/Alpha Engine/src/App.java
@@ -1,10 +1,41 @@
-//import graphics.*;
+import java.nio.IntBuffer;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.glfw.Callbacks;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
 import graphics.window.Window;
+
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 /**
  * Entry Point
  */
 public class App {
+    private long window;
+    
+    public void run(){}
+
+    public void init(){
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if (!GLFW.glfwInit()) {
+            throw new IllegalStateException("Unable to Initialize GLFW");
+        }
+
+        GLFW.glfwDefaultWindowHints();
+        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE);
+    }
+
+    public void loop(){}
+
+
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         Window window = new Window();
