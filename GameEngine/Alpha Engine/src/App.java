ad036d3537ac0806134895524e888197486ce2bc
6546545 <andy.gould46@gmail.com>
2023-12-29 14:24:35 -0500

    .....

--- GameEngine/Alpha Engine/src/App.java
+++ GameEngine/Alpha Engine/src/App.java
@@ -1,87 +1,17 @@
-import java.awt.Graphics;
-import java.nio.IntBuffer;
-import org.lwjgl.opengl.GL;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.system.MemoryStack;
-
-import org.lwjgl.glfw.Callbacks;
-import org.lwjgl.glfw.GLFW;
-import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWVidMode;
-
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
+import graphics.window.Window;
 
 /**
  * Entry Point
  */
 public class App {
-    private long window;
     
-    public void run(){
-        init();
-
-    }
-
-    public void init(){
-        GLFWErrorCallback.createPrint(System.err).set();
-
-        if (!GLFW.glfwInit()) {
-            throw new IllegalStateException("Unable to Initialize GLFW");
-        }
-
-        GLFW.glfwDefaultWindowHints();
-        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE);
-        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE);
-
-        window = GLFW.glfwCreateWindow(640, 480, "Alpha Engine", NULL, NULL);
-        if (window == NULL) {
-            throw new IllegalStateException("Unable to create GLFW Window");
-        }
-        GLFW.glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {});
-        try (MemoryStack stack = stackPush()){
-            IntBuffer pWidth = stack.mallocInt(1);
-            IntBuffer pHeight = stack.mallocInt(1);
-
-            GLFW.glfwGetWindowSize(window, pWidth, pHeight);
-
-            GLFWVidMode vidMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
-
-            GLFW.glfwSetWindowPos(window, (vidMode.width() - pWidth.get(0)) / 2, (vidMode.height() - pHeight.get(0)) / 2);
-
-            GLFW.glfwMakeContextCurrent(window);
-            GLFW.glfwSwapInterval(1);
-            GLFW.glfwShowWindow(window);
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-
-
-
-    }
-
-    public void loop(){
-        GL.createCapabilities();
-
-        while (!GLFW.glfwWindowShouldClose(window)) {
-            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);
-            GLFW.glfwSwapBuffers(window);
-            GLFW.glfwPollEvents();
-        }
-        Callbacks.glfwFreeCallbacks(window);
-        GLFW.glfwDestroyWindow(window);
-
-        GLFW.glfwTerminate();
-        GLFW.glfwSetErrorCallback(null).free();
-
-    }
 
 
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         //new App().run();
-        Graphics.class.Window window = new Window();
+        Window window = new Window();
         window.CreateWindow();
     }
 }
