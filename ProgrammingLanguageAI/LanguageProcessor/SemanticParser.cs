e1ce5a96edc8e9a53a9378a41b8d26c5c6cf654f
6546545 <andy.gould46@gmail.com>
2024-11-14 19:41:44 -0500

    Added basic methods to SemanticParser.cs

---
+++ ProgrammingLanguageAI/LanguageProcessor/SemanticParser.cs
@@ -0,0 +1,11 @@
+namespace ProgrammingLanguageAI.LanguageProcessor
+{
+    public class SemanticParser
+    {
+        public SemanticStructure Parse(string userInput){
+            //Parse User Input
+
+            return new SemanticStructure();
+        }
+    }
+}
