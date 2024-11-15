526e106e526a6f2369e2ff10b3c3c47dbebf62b8
6546545 <andy.gould46@gmail.com>
2024-11-14 19:35:07 -0500

    Added Main to Program.cs

--- ProgrammingLanguageAI/Program.cs
+++ ProgrammingLanguageAI/Program.cs
@@ -1,6 +1,16 @@
-using ProgrammingLanguage 
+using ProgrammingLanguageAI.LanguageProcessor;
+using ProgrammingLanguageAI.Grammar;
+using ProgrammingLanguageAI.IR;
+using ProgrammingLanguageAI.Compiler;
 
 namespace ProgrammingLanguageAI
 {
-    
+    class Program
+    {
+        static void Main(string[] args){
+            var userPrefs = UserInterface.GetUserPreferences();
+
+            var semanticPars = new SemanticParser.Parse(userPreferences);
+        }
+    }
 }
